e1f3f04dcc2b0c6798f2aeffb2d0a0949ef52b01
DoubleDevT <vetminecrafter@gmail.com>
2025-06-04 18:23:08 +0300

    update

---
+++ src/js/render-functions.js
@@ -0,0 +1,61 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const lightbox = new SimpleLightbox('.gallery a');
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+    <li class="gallery-item">
+      <a href="${largeImageURL}">
+        <img class="img" src="${webformatURL}" alt="${tags}" />
+      </a>
+    <div class="info">
+  <div class="info-item">
+    <p class="label">Likes:</p>
+    <p class="value">${likes}</p>
+  </div>
+  <div class="info-item">
+    <p class="label">Views:</p>
+    <p class="value">${views}</p>
+  </div>
+  <div class="info-item">
+    <p class="label">Comments:</p>
+    <p class="value">${comments}</p>
+  </div>
+  <div class="info-item">
+    <p class="label">Downloads:</p>
+    <p class="value">${downloads}</p>
+  </div>
+</div>
+</li>
+  `
+    )
+    .join('');
+
+  galleryList.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryList.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  loader.classList.add('is-hidden');
+}
---
+++ src/main.js
@@ -0,0 +1,41 @@
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import './css/loader.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const query = e.target.elements['search-text'].value.trim();
+  if (!query) {
+    iziToast.warning({ message: 'Enter search term', position: 'topRight' });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+
+    if (data.hits.length === 0) {
+      iziToast.info({ message: 'No images found', position: 'topRight' });
+      return;
+    }
+
+    createGallery(data.hits);
+  } catch (err) {
+    iziToast.error({ message: 'Something went wrong', position: 'topRight' });
+  } finally {
+    hideLoader();
+  }
+});
